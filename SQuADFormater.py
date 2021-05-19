23e90e613d4f71aec34886ee12c79ca3b18110a8
Maciej Krzyszton <maciejkrzyszton63@gmail.com>
2021-05-19 11:08:48 +0200

    backup before changes for commerzbank

--- SQuADFormater.py
+++ SQuADFormater.py
@@ -2,47 +2,73 @@ import json
 import numpy as np
 import uuid
 import array as arr
+import re
 
-
-
-
-
-
-
-
-with open("data3-eval.json") as b:
+with open("data3.json") as b:
     b = json.load(b)
 
-#tmp={"version":"v2.0", "data":{"title":"TK", "paragraphs":{"qas":{"question":"","id":"","answers":{"text":"","answer_start":0},"is_impossible":False},"context":""}}}
-#tmp={"title":"TK", "paragraphs":{"qas":{"question":"","id":"","answers":{"text":"","answer_start":0},"is_impossible":False},"context":""}}
-
 t = list()
-idcounter=0
+idcounter = 0
 
 data = list()
-ans=list()
+ans = list()
 qas = list()
+question = list()
+context = ''
+
 for qa in b:
-    print(qa["header"])
-    print(qa["body"])
 
+    bodyString=qa["body"]
+    headerString=qa["header"]
 
-    question = {"question":qa["header"],"id":idcounter,"answers":ans,"is_impossible":False}
+    context += headerString
+    context += bodyString
+    #context += ' '
+    context += ' '
 
-    qas.append({"qas":question,"context":qa["body"]})
+with open("context.txt", "w") as new_b:
+    new_b.write(context)
 
-    idcounter=idcounter+1
+for qa in b:
+    bodyString=qa["body"]
+    headerString=qa["header"]
 
-ans.append({"text":"","answer_start":0})
+    #absolutePosition = re.search(bodyString, context)
+    absolutePosition = context.find(bodyString)
 
-#titlePara = {"title": "TK", "paragraphs": t}
+    if absolutePosition is not None:
+        absolutePosition = absolutePosition + 1
+        #absolutePosition = absolutePosition.start()+1
+    else:
+        print("absolutePosition with regex could not be determined, following has been caught: ")
+        print("<------------------------------->")
+        print(headerString)
+        print(bodyString)
+        print("---------------------------------")
+        print("Retrying with find() of string")
+        print("<------------------------------->")
+        #absolutePosition = context.find(bodyString)
+        #absolutePosition=absolutePosition+1
+        absolutePosition = re.search(bodyString, context)
+        absolutePosition = absolutePosition.start() + 1
+
+    ans.append({"text": bodyString, "answer_start": absolutePosition})
+    ans.append({"text": bodyString, "answer_start": absolutePosition})
+    ans.append({"text": bodyString, "answer_start": absolutePosition})
+    ans.append({"text": bodyString, "answer_start": absolutePosition})
+
+    question.append({"question": headerString, "id": idcounter, "answers": ans, "is_impossible": False})
+    idcounter = idcounter + 1
+    ans = list()
+
+qas.append({"qas": question, "context": context})
+# titlePara = {"title": "TK", "paragraphs": t}
 titlePara = {"title": "TK", "paragraphs": qas}
 
-tt=list()
+tt = list()
 tt.append(titlePara)
 
-test={"version":"v2.0", "data": tt}
+test = {"version": "v2.0", "data": tt}
 
-with open("tKSQuADFormat-eval.json", "w") as new_b:
-    json.dump(test, new_b, indent=1, separators=(',', ': '),ensure_ascii=False)
-
+with open("oneContextTrain.json", "w") as new_b:
+    json.dump(test, new_b, indent=1, separators=(',', ': '), ensure_ascii=False)
